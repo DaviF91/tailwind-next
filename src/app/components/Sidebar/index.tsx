6f2a9b679856a71ae48be991c322e18871aba48e
Davi Ferreira <daviferreiraaew@gmail.com>
2023-08-08 20:55:59 -0300

    Widget de Espaço Usado

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -2,6 +2,7 @@ import { Logo } from "./Logo";
 import { Search, Home, BarChart, SquareStack, CheckSquare, Flag, Users, LifeBuoy, Cog } from "lucide-react" //biblioteca de icones  npm i lucide-react 
 
 import { NavItem } from "./NavItem";
+import { UsedSpaceWidget } from "./UsedSpaceWidget";
 
 
 export function Sidebar() {
@@ -29,24 +30,7 @@ export function Sidebar() {
           <NavItem title="Settings" icon={Cog} />
         </nav>
 
-        <div className="flex flex-col gap-4 rounded-lg bg-violet-50 py-5 px-4 ">
-          <div className="space-y-1"></div>
-            <span className="text-sm/5 font-medium text-violet-700">Used space</span>
-            <p className="text-sm/5 text-violet-500">Your team has used 80% of your available space. Need more?</p>
-        </div>
-
-        <div className="h-2 rounded-full bg-violet-100">
-          <div className="h-2 w-4/5 rounded-full bg-violet-600"/>
-        </div>
-
-        <div className="spac-x-3">
-          <button type="button" className="text-sm font-medium text-violet-500 hover:text-violet-600">
-            Dismiss
-          </button>
-          <button type="button" className="text-sm font-medium text-violet-700 hover:text-violet-900">
-            Upgrade
-          </button>
-        </div>
+        <UsedSpaceWidget/>
       </div>
     </aside>
   )
